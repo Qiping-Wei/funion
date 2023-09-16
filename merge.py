b2601663363f0fac49c9d6f3f1cc9540f4edc4c9
Qiana <qipingwei0223@163.com>
2023-09-16 17:59:37 -0500

    init

---
+++ merge.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from funion.cli import main
+
+if __name__ == "__main__":
+
+    """
+  example arguments provided to merge.py:  
+--solidity_file_path
+.\contracts\finance\
+--solidity_file_name
+VestingWallet.sol
+--contract_name
+VestingWallet
+--solc_version
+0.8.0
+--import_paths
+.\contracts\
+--result_path
+.
+    """
+    main()
+    exit()
